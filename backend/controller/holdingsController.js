441130dc45ba8635bd159ec276361e555212b532
Sarthak Vyas <89727581+sarthakvyas16@users.noreply.github.com>
2025-07-17 14:34:16 +0530

    Delete backend directory

--- backend/controller/holdingsController.js
+++
@@ -1,6 +0,0 @@
-const { HoldingsModel } = require("../model/HoldingsModel");
-
-module.exports.index = async (req, res) => {
-  let allHoldings = await HoldingsModel.find({});
-  res.json(allHoldings);
-};
--- backend/controller/positionsController.js
+++
@@ -1,6 +0,0 @@
-const { PositionsModel } = require("../model/PositionsModel");
-
-module.exports.index = async (req, res) => {
-  let allPositions = await PositionsModel.find({});
-  res.json(allPositions);
-};
